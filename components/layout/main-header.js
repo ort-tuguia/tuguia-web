6692c0d07c300fb58287a33f59535e2a76f4d46a
timaenho <tom.maenhout@gmail.com>
2022-05-07 13:05:54 -0300

    nav-bar-link

--- components/layout/main-header.js
+++ components/layout/main-header.js
@@ -2,22 +2,11 @@ import Link from "next/link";
 import classes from "./main-header.module.css";
 function MainHeader() {
   return (
-    <header>
-      {/* <div className={classes.logo}>
-        <Link href="/">TuGuia</Link>
-      </div>
-      <nav className={classes.navigation}>
-        <ul>
-          <li>
-            <Link href="/Perfil">Perfil</Link>
-          </li>
-          <li>
-            <Link href="/Reservas">Reservas</Link>
-          </li>
-        </ul>
-      </nav> */}
+ 
 
-      <nav className="flex items-center justify-between flex-wrap bg-teal-700 p-6">
+
+<header>
+       <nav className="flex items-center justify-between flex-wrap bg-teal-700 p-6">
         <div className="flex items-center flex-shrink-0 text-white mr-6">
           <svg
             className="fill-current h-8 w-8 mr-2"
@@ -26,9 +15,67 @@ function MainHeader() {
             viewBox="0 0 54 54"
             xmlns="http://www.w3.org/2000/svg"
           >
-           {/*  <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" /> */}
+           </svg>
+          <span className="font-semibold text-xl tracking-tight">TuGuia Turistas</span>
+        </div>
+        <div className="block lg:hidden">
+          <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+            <svg
+              className="fill-current h-3 w-3"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
+          </button>
+        </div>
+        <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+          <div className="text-sm lg:flex-grow">
+            <a
+              href="/Turista/HomeTurista/1"
+              className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+            >
+              Buscar
+            </a>
+            <a
+              href="#responsive-header"
+              className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+            >
+              Add Servicio
+            </a>
+            <a
+              href="#responsive-header"
+              className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
+            >
+              Reservas
+            </a>
+          </div>
+          <div>
+            <a
+              href="/auth"
+              className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+            >
+              Login
+            </a>
+          </div>
+        </div>
+      </nav>
+    </header> 
+
+/*     <header>
+
+<nav className="flex items-center justify-between flex-wrap bg-teal-700 p-6">
+        <div className="flex items-center flex-shrink-0 text-white mr-6">
+          <svg
+            className="fill-current h-8 w-8 mr-2"
+            width="54"
+            height="54"
+            viewBox="0 0 54 54"
+            xmlns="http://www.w3.org/2000/svg"
+          >
           </svg>
-          <span className="font-semibold text-xl tracking-tight">TuGuia</span>
+          <span className="font-semibold text-xl tracking-tight">TuGuia Guia</span>
         </div>
         <div className="block lg:hidden">
           <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
@@ -48,13 +95,13 @@ function MainHeader() {
               href="#responsive-header"
               className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
             >
-              Servicios
+              <Link href="/Guia/HomeGuia/1">Mis Servicios</Link>
             </a>
             <a
               href="#responsive-header"
               className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
             >
-              Add Servicio
+               <Link href="/Guia/AgregarServicio/1">Agregar Servicio</Link>
             </a>
             <a
               href="#responsive-header"
@@ -68,12 +115,17 @@ function MainHeader() {
               href="#"
               className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
             >
-              Login
+               <Link href="/auth">Login</Link> 
             </a>
           </div>
         </div>
       </nav>
-    </header>
+
+    </header> */
+
+
+
+
   );
 }
 
