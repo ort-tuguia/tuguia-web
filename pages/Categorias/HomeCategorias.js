4dd6d0716d71e32040063360ee4c0c62ce0f4a59
alejandrogrosso <alematiasgrosso@gmail.com>
2022-06-16 20:30:11 -0300

    Fix CreateCategory

--- pages/Categorias/HomeCategorias.js
+++ pages/Categorias/HomeCategorias.js
@@ -99,12 +99,12 @@ function HomeCategorias(){
                                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
                                                required/>
                                     </div>
-                                    <button className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                                    <button type="button" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                                     onClick={()=>{
                                          var nameCategory = document.getElementById("name").value
                                         var descriptionCategory = document.getElementById("description").value
-
                                         CrearLaCat(nameCategory,descriptionCategory)
+                                        document.getElementById("authentication-modal").style.display = "none";
                                     }}
                                     >Crear categoria</button>
                                 </form>
