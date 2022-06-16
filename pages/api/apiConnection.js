36d215cef37dc91e71a2b79ee6ab50965c9f437e
alejandrogrosso <alematiasgrosso@gmail.com>
2022-06-15 21:21:38 -0300

    Update apiConnection.js

--- pages/api/apiConnection.js
+++ pages/api/apiConnection.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 const apiConnection = axios.create({
-   baseURL:'http://localhost:8080/api',
-    // baseURL : 'https://ort-tuguia-api.herokuapp.com/api',
+
+     baseURL : 'https://ort-tuguia-api.herokuapp.com/api',
     headers:{
         Accept: 'application/json',
         'Content-Type': 'application/json'
