7f494f42abe91d88627c3b9acb3ac2da5f24a6d7
alejandrogrosso <alematiasgrosso@gmail.com>
2022-06-15 21:48:28 -0300

    Delete Users

--- pages/api/apiUsuarios.js
+++ pages/api/apiUsuarios.js
@@ -11,5 +11,16 @@ export default {
         })
 
         return response
+    },
+    async deleteUsuarios(username, token) {
+        console.log("Token en deleteUsuarios " + token)
+        console.log("Username en deleteUsuarios " + username)
+        let response = await apiConnection.delete(`/users/${username}`, {
+            headers: {
+                Authorization: token ,
+                Accept: 'application/json',
+            }
+        })
+        return response
     }
 }
