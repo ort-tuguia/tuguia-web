989c8e677ce5be1cf6824a85480c7dc697ad490c
alejandrogrosso <alematiasgrosso@gmail.com>
2022-06-18 19:01:56 -0300

    Update ChangePassword.js

--- pages/Admin/HomeAdmin/ChangePassword.js
+++ pages/Admin/HomeAdmin/ChangePassword.js
@@ -5,6 +5,8 @@ import {Modal} from "flowbite-react";
 function ChangePassword(props){
     const[password, setPassword] = useState("");
     const[confirmPassword, setConfirmPassword] = useState("");
+    const[flag, setFlag] = useState(false);
+
     useEffect(() => {
         setPassword(props.password);
     },[props.password])
@@ -32,18 +34,26 @@ function ChangePassword(props){
                                     <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Contraseña</label>
                                     <input type="password" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)}
                                              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                                placeholder="Ingrese la contraseña" required/>
+                                                placeholder="Ingrese la contraseña" required />
+                                    {password=== ""&& flag &&  <p className="text-red-500 text-sm">Por favor coloque la nueva clave</p>}
                                     </div>
                                 <div class="mb-6">
                                     <label htmlFor="confirmPassword" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Confirmar Contraseña</label>
                                     <input type="password" id="confirmPassword" name="confirmPassword" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}
                                                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                                 placeholder="Confirme la contraseña" required/>
+                                    {confirmPassword=== ""&& flag && <p className="text-red-500 text-sm">Por favor confirme la clave</p>}
+
                                 </div>
+                                {password !== confirmPassword && <span className="text-red-500">Las contraseñas no coinciden</span>}
                                 <button type="button" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                                     onClick={() =>{
 
-                                        props.onChangePassword(props.username,password,confirmPassword)
+                                        if(password === "" || confirmPassword === "") {
+                                            setFlag(true)
+                                        }else{
+                                            props.onChangePassword(props.username, password, confirmPassword)
+                                        }
 
                                 }}>
                                     Cambiar Contraseña
